2a7e588d2933c3ec7fec03bbbda28c9928effeb6
katherinborda <katherin.borda73@gmail.com>
2022-11-11 17:57:34 -0500

    se modifica el controlador resultados,incluyendo mesa en la funcion crear

--- Controladores/ControladorResultado.py
+++ Controladores/ControladorResultado.py
@@ -20,12 +20,14 @@ class ControladorResultado():
     """
     Asignacion candidato y partido a resultado
     """
-    def create(self,infoResultado,id_candidato,id_partido):
+    def create(self,infoResultado,id_candidato,id_partido,id_mesa):
         nuevoResultado= Resultado(infoResultado)
         elCandidato=Candidato(self.repositorioCandidato.findById(id_candidato))
         elpartido=Partido(self.repositorioPartido.findById(id_partido))
+        elMesa=Mesa (self.repositorioMesa.findById(id_mesa))
         nuevoResultado.candidato=elCandidato
         nuevoResultado.partido=elpartido
+        nuevoResultado.mesa= elMesa
         return self.repositorioResultado.save(nuevoResultado)
     def show(self,id):
         elResultado=Resultado(self.repositorioResultado.findById(id))
@@ -33,13 +35,15 @@ class ControladorResultado():
     """
     Modificación de Resultado (candidato y partido)
     """
-    def update(self,id,infoResultado,id_candidato,id_partido):
+    def update(self,id,infoResultado,id_candidato,id_partido,id_mesa):
         elResultado=Resultado(self.repositorioResultado.findById(id))
-        elResultado.nmesa=infoResultado["nmesa"]
+        elResultado.voto=infoResultado["voto"]
         elCandidato=Candidato(self.repositorioCandidato.findById(id_candidato))
         elpartido=Partido(self.repositorioPartido.findById(id_partido))
+        elMesa = Mesa(self.repositorioMesa.findById(id_mesa))
         elResultado.candidato = elCandidato
         elResultado.partido = elpartido
+        elResultado.mesa = elMesa
         return self.repositorioResultado.save(elResultado)
 
     def delete(self, id):
